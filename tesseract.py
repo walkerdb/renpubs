32709440cae586bef9db9df1d42973602f3e6fa7
walkerdb <wboyle13@gmail.com>
2017-03-19 07:23:53 -0600

    run with ita_old, ita, ger, and fra together

--- tesseract.py
+++ tesseract.py
@@ -5,12 +5,14 @@ from tqdm import tqdm
 root_path = "/Volumes/Samsung USB/nv images/out"
 images = [image for image in os.listdir(root_path) if image.endswith(".tif")]
 
+consonants = ["bcdfghjklmnpqrstvwxz"]
+
 for image in tqdm(images):
     input_path = os.path.join(root_path, image)
     output_path = "out/" + image.split(".")[0]
 
     process = subprocess.Popen(
-        ["tesseract", input_path, output_path, "--psm", "4", "-l", "ita_old"],
+        ["tesseract", input_path, output_path, "--psm", "4", "-l", "ita_old+ita+fra+ger"],
         shell=False,
         stdout=subprocess.PIPE,
         stderr=subprocess.PIPE
