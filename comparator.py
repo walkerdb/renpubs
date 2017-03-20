f56034d2985dc452cffea54ba5aa9153c0ecabf2
walkerdb <wboyle13@gmail.com>
2017-03-20 11:46:39 -0400

    add page correction submit functionality

--- comparator.py
+++ comparator.py
@@ -1,6 +1,6 @@
 import base64
 import os
-from flask import Flask, render_template, render_template_string
+from flask import Flask, render_template, request, redirect, url_for
 from PIL import Image
 
 app = Flask(__name__, static_path="/static")
@@ -12,19 +12,25 @@ TXT_ROOT = "out"
 def index():
     return "hi"
 
-@app.route("/<imageNum>")
+@app.route("/<imageNum>", methods=["GET", "POST"])
 def page(imageNum):
     imgUrl = "https://earlyaudio.s3.amazonaws.com/nv/{}.png".format(imageNum)
-
     textPath = "{}/{}.txt".format(TXT_ROOT, imageNum)
 
-    with open(textPath) as f:
-        text = f.read()
+    if request.method == "POST" and request.form.get("text"):
+        with open(textPath, "w") as f:
+            f.write(request.form.get("text").replace("\r\n", "\n"))
 
-    nextPage = str(int(imageNum) + 1).zfill(4)
-    prevPage = str(int(imageNum) - 1).zfill(4) if int(imageNum) >= 2 else None
+        return redirect(url_for("page", imageNum=imageNum))
 
-    return render_template("page.html", text=text, imgUrl=imgUrl, nextPage=nextPage, prevPage=prevPage)
+    else:
+        with open(textPath) as f:
+            text = f.read()
+
+        nextPage = str(int(imageNum) + 1).zfill(4)
+        prevPage = str(int(imageNum) - 1).zfill(4) if int(imageNum) >= 2 else None
+
+        return render_template("page.html", text=text, imgUrl=imgUrl, nextPage=nextPage, prevPage=prevPage, currentPage=imageNum)
 
 
 app.run()
