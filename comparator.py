78322180cad8839af930de82c24ca0573a69d962
walkerdb <wboyle13@gmail.com>
2017-03-20 20:31:18 -0400

    assorted corrections

--- comparator.py
+++ comparator.py
@@ -17,18 +17,18 @@ def page(imageNum):
     imgUrl = "https://earlyaudio.s3.amazonaws.com/nv/{}.png".format(imageNum)
     textPath = "{}/{}.txt".format(TXT_ROOT, imageNum)
 
+    nextPage = str(int(imageNum) + 1).zfill(4)
+    prevPage = str(int(imageNum) - 1).zfill(4) if int(imageNum) >= 2 else None
+
     if request.method == "POST" and request.form.get("text"):
         with open(textPath, "w") as f:
             f.write(request.form.get("text").replace("\r\n", "\n"))
 
-        return redirect(url_for("page", imageNum=imageNum))
+        return redirect(url_for("page", imageNum=nextPage))
 
     else:
         with open(textPath) as f:
             text = f.read()
-
-        nextPage = str(int(imageNum) + 1).zfill(4)
-        prevPage = str(int(imageNum) - 1).zfill(4) if int(imageNum) >= 2 else None
 
         return render_template("page.html", text=text, imgUrl=imgUrl, nextPage=nextPage, prevPage=prevPage, currentPage=imageNum)
 
