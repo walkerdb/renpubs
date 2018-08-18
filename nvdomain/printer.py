3069d92270240d797f4f60cfb2771b43d52a950c
walkerdb <wboyle13@gmail.com>
2018-08-18 12:51:03 -0500

    add printers

--- nvdomain/printer.py
+++ nvdomain/printer.py
@@ -1,4 +1,7 @@
-class Printer(object):
+from util.field_equality_mixin import FieldEqualityMixin
+
+
+class Printer(FieldEqualityMixin):
     def __init__(self, name, primary_location=None):
         self.name = name
         self.primary_location = primary_location
